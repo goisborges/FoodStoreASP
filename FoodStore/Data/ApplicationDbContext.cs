a5ee1c66d5c25bbf1f2445c1f642414bb5da56cd
Marcos de Gois Borges <61516804+goisborges@users.noreply.github.com>
2021-09-29 22:39:40 -0400

    ApplicationDbContext.cs updated with models references. Connection string created @ appsettings.json. NuGet package SQLServer instaled. Modified UseSqlite to UseSqlServer @ Startup.cs

--- FoodStore/Data/ApplicationDbContext.cs
+++ FoodStore/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FoodStore.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,13 @@ namespace FoodStore.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        //add global refs for all models so they are available widely
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
