738ca5c8d682218347e9838e99d6bed46d45893a
Marcos de Gois Borges <61516804+goisborges@users.noreply.github.com>
2021-11-09 21:01:41 -0500

    implemented the ShopController and View. Had a bug on my computer and lost data.

---
+++ FoodStore/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using FoodStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}
